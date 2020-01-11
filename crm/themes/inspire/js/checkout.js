ca472468b6ab0bb27f66cd576868dc980e41c483
Norman Witte III <norm.witte@gmail.com>
2020-01-11 01:01:30 -0500

    window resize feature for iframe

--- crm/themes/inspire/js/checkout.js
+++ crm/themes/inspire/js/checkout.js
@@ -12,6 +12,10 @@ window.onload = function () {
 
     iframe.contentWindow.postMessage({ token: token, route: 'membership-info' }, '*');
 
+    window.addEventListener('message', function (event) {
+        if (event.data.height) iframe.style.height = (event.data.height + 20) + 'px';
+    });
+
     function getCookieValue(a) {
         var b = document.cookie.match('(^|[^;]+)\\s*' + a + '\\s*=\\s*([^;]+)');
         return b ? b.pop() : '';
