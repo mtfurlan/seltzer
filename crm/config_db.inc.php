ab88675b09cf128a04a19f6acf178af42dd530ac
Mark Furland <mark@fur.land>
2023-10-05 23:43:29 -0400

    simplify and make it work

---
+++ crm/config_db.inc.php
@@ -0,0 +1,6 @@
+<?php
+$config_db_host = getenv('MYSQL_HOST');
+$config_db_socket = null;
+$config_db_user = getenv('MYSQL_USER');
+$config_db_password = getenv('MYSQL_PASSWORD');
+$config_db_db = getenv('MYSQL_DATABASE');
